cb30bf6d185ef9c0ad5129cff7f9f890fd4149e6
Kevin <kpascualdelrio@gmail.com>
2021-10-16 11:03:25 +0200

    server con index.html incluido

--- server/router/router.js
+++ server/router/router.js
@@ -1,6 +1,18 @@
+//Modulo dentro de enrutado
+const fs = require('fs');
+const HTML_CONTENT_TYPE ="text/html";
 
 exports.init = function(req,res){
 res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello Kevin');
+res.setHeader('Content-Type', HTML_CONTENT_TYPE);
+const index = fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
+    if(err){
+        console.log("error en la carga de index.html");
+        res.end("error en la carga");
+       
+    }else{
+        res.end(data);
+    }
+  });
+
 }
