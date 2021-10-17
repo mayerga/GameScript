825f237020419801329c10435cb17989f7ca90eb
Kevin <kpascualdelrio@gmail.com>
2021-10-17 09:55:22 +0200

    Pagina web operativa montada en el servidor (sin bootstrap)

--- server/router/router.js
+++ server/router/router.js
@@ -1,6 +1,8 @@
 //Modulo dentro de enrutado
 const fs = require("fs");
 const HTML_CONTENT_TYPE = "text/html";
+const CSS_CONTENT_TYPE = "text/css"
+const IMG_CONTENT_TYPE = "text/img"
 
 exports.init = function (req, res) {
   res.statusCode = 200;
@@ -24,7 +26,37 @@ exports.init = function (req, res) {
         }
       }
     );
-  } else {
+  }
+  //Añadido elementos carpeta IMG P.W
+  else if (path.match("\.img$")){
+    res.setHeader("Content-Type",IMG_CONTENT_TYPE);
+      const css = fs.readFile(
+        `${__dirname}/../public/`+path,
+        (err, data) => {
+          if (err) {
+            console.log("error en la carga de IMG");
+            res.end("error en la carga de" +path);
+          } else {
+            res.end(data);
+          }
+        }
+      );
+  } 
+  //Añadido elementos carpeta CSS P.W
+  else if (path.match("\.css$")){
+    res.setHeader("Content-Type",CSS_CONTENT_TYPE);
+      const css = fs.readFile(
+        `${__dirname}/../public/`+path,
+        (err, data) => {
+          if (err) {
+            console.log("error en la carga de CSS");
+            res.end("error en la carga de" +path);
+          } else {
+            res.end(data);
+          }
+        }
+      );
+      } else {
     res.setHeader("Content-Type", HTML_CONTENT_TYPE);
     res.end("Otra Cualquiera");
   }
