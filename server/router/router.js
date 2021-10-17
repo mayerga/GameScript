f9348d34d12afc681c8ce106635cf456f71f9ca6
Kevin <kpascualdelrio@gmail.com>
2021-10-17 09:35:19 +0200

    AñadidoComentarios + Path

--- server/router/router.js
+++ server/router/router.js
@@ -1,18 +1,31 @@
 //Modulo dentro de enrutado
-const fs = require('fs');
-const HTML_CONTENT_TYPE ="text/html";
+const fs = require("fs");
+const HTML_CONTENT_TYPE = "text/html";
 
-exports.init = function(req,res){
-res.statusCode = 200;
-res.setHeader('Content-Type', HTML_CONTENT_TYPE);
-const index = fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
-    if(err){
-        console.log("error en la carga de index.html");
-        res.end("error en la carga");
-       
-    }else{
-        res.end(data);
-    }
-  });
+exports.init = function (req, res) {
+  res.statusCode = 200;
 
-}
+  //path
+  const path = req.url;
+  //console.log(path);
+
+  //Enrutado
+  if (path === "/") {
+    res.setHeader("Content-Type", HTML_CONTENT_TYPE);
+
+    const index = fs.readFile(
+      `${__dirname}/../public/index.html`,
+      (err, data) => {
+        if (err) {
+          console.log("error en la carga de index.html");
+          res.end("error en la carga");
+        } else {
+          res.end(data);
+        }
+      }
+    );
+  } else {
+    res.setHeader("Content-Type", HTML_CONTENT_TYPE);
+    res.end("Otra Cualquiera");
+  }
+};
