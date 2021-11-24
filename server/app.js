f2d9efe2f5881ad6ea5e41a94bf1c5554fe21001
JulitoxD <91586835+JulitoxD@users.noreply.github.com>
2021-11-24 11:56:34 +0100

    Cambiando arquitectura

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express   = require("express");   //Importamos la librería de Express.
+const app       = express();            //Guadamos la librería en una variable.
+const port      = 3000;
+
+// Static Files
+app.use(express.static('public'));
+app.use('/public/css', express.static(__dirname + '/public/css'));
+app.use('/public/js', express.static(__dirname + '/public/js'));
+app.use('/public/img', express.static(__dirname + '/public/img'));
+
+//Views
+app.use(require('./html'));
+
+app.listen(port, function () {
+    console.info("Servidor", port, "en funcionamiento");
+});
