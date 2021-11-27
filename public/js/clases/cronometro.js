fc24a399d11516148c4e4099126b7e0eb6e9af9c
Kpascualdelrio <kpascual@uoc.edu>
2021-11-27 14:15:55 +0100

    Cronometro y playCronometro

--- public/js/clases/cronometro.js
+++ public/js/clases/cronometro.js
@@ -1,26 +1,62 @@
+class cronometro {
+  constructor() {
+    this._segundos;
+  }
 
-class cronometro{
-    constructor(/*enum-SOLO PERMITIDO CON TYPESCRIPT*/){
-        this.enum=this.enum;
-    }
-    /*DEFINIMOS GETTERS & SETTERS */
-    set enum(valor){
-        this.enum=valor;
-    }
-    get enum(){
-        return this.enum;
-    }
-    /*DEFINIMOS METODOS */
-    retrocederTiempor(){
+  get segundos() {
+    return this._segundos;
+  }
 
-    }
-    reiniciarCronometro(){
-        
+  set segundos(segundos) {
+    this._segundos = segundos;
+  }
+
+  inicializaCronometro() {
+
+    let secInicio = 10;
+    this._segundos = secInicio;
+
+    return this._segundos;
+  }
+
+  cuentaAtrasCronometro(param) {
+    
+    while(this._segundos >=0){
+    param.innerHTML = this._segundos;
+    this._segundos--;
+    //console.log(this._segundos + "hola");
+    }   
+  }
+
+  reiniciaCronometro(){
+
+    if (this._segundos == 0){
+
+        alert("Se acabo el tiempo! Cambio de Turno" );
     }
 
+    this.inicializaCronometro();
+  }
 }
-        
 
+/*------------------------------------------------------------------*/
 
+//     constructor(/*enum-SOLO PERMITIDO CON TYPESCRIPT*/){
+//         this.enum=this.enum;
+//     }
+//     /*DEFINIMOS GETTERS & SETTERS */
+//     set enum(valor){
+//         this.enum=valor;
+//     }
+//     get enum(){
+//         return this.enum;
+//     }
+//     /*DEFINIMOS METODOS */
+//     retrocederTiempor(){
 
+//     }
+//     reiniciarCronometro(){
 
+//     }
+
+// }
---
+++ public/js/clases/playCronometro.js
@@ -0,0 +1,21 @@
+
+function autoplay(){
+    let cronoJugador = new cronometro();
+    let seconds = document.getElementById('seconds');
+    let inicio = cronoJugador.inicializaCronometro();
+    seconds.innerHTML = inicio;
+
+    let atras = window.setInterval(cronoJugador.cuentaAtrasCronometro(seconds), 1000);
+    
+    
+
+    
+
+    //seconds.innerHTML = cronoJugador.inicializaCronometro();
+
+
+    // cronoJugador.cuentaAtrasCronometro(seconds);
+    // cronoJugador.reiniciaCronometro();
+}
+
+autoplay();
