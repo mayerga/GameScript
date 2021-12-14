1663bc7ed44cf1214e61189f8d83307cc90e2566
JulitoxD <91586835+JulitoxD@users.noreply.github.com>
2021-12-14 16:12:44 +0100

    Ni idea de lo que estoy haciendo

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express   = require("express");   //Importamos la librería de Express.
-const router    = require("./routes")
+const router    = require("./routes/html")
 const app       = express();            //Guadamos la librería en una variable.
 const port      = 3000;
+
+app.use(express.static('public'));
+app.use('/public/css', express.static(__dirname + '/public/css'));
+app.use('/public/js', express.static(__dirname + '/public/js'));
+app.use('/public/img', express.static(__dirname + '/public/img'));
 
 app.use(router);
 
